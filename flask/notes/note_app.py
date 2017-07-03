50bf9842d7260b9b29b9c7e80056a2c01ea83279
Simon Hofmann <mail@simon-hofmann.org>
2017-07-03 14:49:40 +0200

    Checkpoint for web forms

--- flask/notes/note_app.py
+++ flask/notes/note_app.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
-__all__ = ['app']
+__all__ = ["app"]
 
 app = Flask(__name__)
+Bootstrap(app)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return "<h1>Hello ConSolis!</h1>"
+    return render_template("index.html", elements=range(12))
 
-@app.route('/hello/<string:username>')
-def user(username):
+@app.route("/hello/<string:username>")
+def say_hi(username):
     return render_template("hello.html", user=username)
+
+@app.route("/user/<string:username>")
+def user(username):
+    pass
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
